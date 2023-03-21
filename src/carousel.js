a7130a1d15f02e103eba185b0a5a220dbae93ddd
marshall <113690263+marshallmhamad@users.noreply.github.com>
2023-03-21 16:38:37 +0300

    created portfolio

---
+++ src/carousel.js
@@ -0,0 +1,40 @@
+import React from "react";
+
+function Carousel() {
+  return (
+    <div className="wrapper">
+      <div className="slider">
+        <div className="slide">
+          <img src="../vscode.jpg" alt="" />
+          <img src="../git.png" alt="" />
+          <img src="../node.png" alt="" />
+          <img className="bigger" src="../express.png" alt="" />
+          <img src="../mongo.png" alt="" />
+          <img src="../react.png" alt="" />
+          <img src="../redux.jfif" alt="" />
+          <img src="../context.png" alt="" />
+          <img className="bigger" src="../nextjs.png" alt="" />
+          <img src="../sass.png" alt="" />
+          <img className="bigger" src="../firebase.png" alt="" />
+          <img src="../strapi.jpg" alt="" />
+        </div>
+        <div className="slide">
+          <img src="../vscode.jpg" alt="" />
+          <img src="../git.png" alt="" />
+          <img src="../node.png" alt="" />
+          <img className="bigger" src="../express.png" alt="" />
+          <img src="../mongo.png" alt="" />
+          <img src="../react.png" alt="" />
+          <img src="../redux.jfif" alt="" />
+          <img src="../context.png" alt="" />
+          <img className="bigger" src="../nextjs.png" alt="" />
+          <img src="../sass.png" alt="" />
+          <img className="bigger" src="../firebase.png" alt="" />
+          <img src="../strapi.jpg" alt="" />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Carousel;
